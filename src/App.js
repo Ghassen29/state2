be09ec8c71e81a950d2ebb9fb63640c591f860d8
ghassen mejri <[ghasen29@gmail.com]>
2022-10-12 15:45:08 +0100

    state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React , {Component} from 'react'
+import Profil from './Profil';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export class App extends Component{
+
+constructor(){
+  super();
+  this.state={
+    show:false,
+    
+  }
 }
 
-export default App;
+toggel=()=>{
+  this.setState({  show: !this.state.show  })
+}
+
+
+ render(){
+  return (
+    <div>
+      
+
+     
+
+
+      <button className='button' onClick={()=>this.toggel()} >Show-Hide</button>
+      <h1>{this.state.name}</h1>
+      {  this.state.show  &&  <Profil/>  }
+     
+      
+    </div>
+  )
+}
+}
+
+export default App
